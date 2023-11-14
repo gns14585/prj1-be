e98967c1f1fc6fca547e1ec15e0d673aefd9f808
이정훈 <gns14585@naver.com>
2023-11-14 11:43:58 +0900

    게시글 수정시, 탈퇴시 권한확인

--- src/main/java/com/example/prj1be/controller/MemberController.java
+++ src/main/java/com/example/prj1be/controller/MemberController.java
@@ -75,9 +75,15 @@ public class MemberController {
     }
 
     @DeleteMapping
-    public ResponseEntity delete(String id) {
-        // TODO : 로그인 했는지? -> 안했으면 401
-        // TODO : 자기 정보인지? -> 아니면 403
+    public ResponseEntity delete(String id,
+                                 @SessionAttribute(value = "login", required = false) Member login) {
+        if (login == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        if (!service.hasAccess(id, login)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
 
         if (service.deleteMember(id)) {
             return ResponseEntity.ok().build();
--- src/main/java/com/example/prj1be/service/MemberService.java
+++ src/main/java/com/example/prj1be/service/MemberService.java
@@ -88,20 +88,7 @@ public class MemberService {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public boolean hasAccess(String id, Member login) {
+        return login.getId().equals(id);
+    }
 }
