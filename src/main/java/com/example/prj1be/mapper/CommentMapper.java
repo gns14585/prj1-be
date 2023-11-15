acd3c3247865889129f53be5cdb4e2c49b2784ff
LeeJungHoon <gns14585@naver.com>
2023-11-15 14:10:58 +0900

    댓글 삭제 기능

--- src/main/java/com/example/prj1be/mapper/CommentMapper.java
+++ src/main/java/com/example/prj1be/mapper/CommentMapper.java
@@ -1,6 +1,7 @@
 package com.example.prj1be.mapper;
 
 import com.example.prj1be.domin.Comment;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -22,4 +23,10 @@ public interface CommentMapper {
             WHERE boardId = #{boardId}
             """)
     List<Comment> selectByBoardId(Integer boardId);
+
+    @Delete("""
+            DELETE FROM comment
+            WHERE id = #{id}
+            """)
+    int deleteById(Integer id);
 }
