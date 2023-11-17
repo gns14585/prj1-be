0d2f2d0b8a2f78a4261f0d5316d7494e0e933b74
LeeJungHoon <gns14585@naver.com>
2023-11-17 17:47:13 +0900

    현재 페이지 추가

--- src/main/java/com/example/prj1be/service/BoardService.java
+++ src/main/java/com/example/prj1be/service/BoardService.java
@@ -46,27 +46,25 @@ public class BoardService {
         Map<String, Object> map = new HashMap<>();
         Map<String, Object> pageInfo = new HashMap<>();
 
-        // 총 게시물 숫자
         int countAll = mapper.countAll();
-
-        // 마지막번호 페이지 파악
         int lastPageNumber = (countAll - 1) / 10 + 1;
-
-        // 시작번호 페이지 (왼쪽으로 넘어가는)
-        int startPageNumber = (page - 1) / 10 * 10 + 1 ;
-
-        // 마지막번호 (오른쪽으로 넘어가는)
+        int startPageNumber = (page - 1) / 10 * 10 + 1;
         int endPageNumber = startPageNumber + 9;
-
-        // 오른쪽 끝으로 넘어가는 페이지 번호
         endPageNumber = Math.min(endPageNumber, lastPageNumber);
+        int prevPageNumber = startPageNumber - 10;
+        int nextPageNumber = endPageNumber + 1;
 
+        pageInfo.put("currentPageNumber", page);
         pageInfo.put("startPageNumber", startPageNumber);
         pageInfo.put("endPageNumber", endPageNumber);
+        if (prevPageNumber > 0) {
+            pageInfo.put("prevPageNumber", prevPageNumber);
+        }
+        if (nextPageNumber <= lastPageNumber) {
+            pageInfo.put("nextPageNumber", nextPageNumber);
+        }
 
-        // 10페이지씩 보이게
         int from = (page - 1) * 10;
-
         map.put("boardList", mapper.selectAll(from));
         map.put("pageInfo", pageInfo);
         return map;
@@ -77,7 +75,7 @@ public class BoardService {
     }
 
     public boolean remove(Integer id) {
-        // 1. 게시물에 달린 댓글들 지우기
+        // 게시물에 달린 댓글들 지우기
         commentMapper.deleteByBoardId(id);
 
         // 좋아요 레코드 지우기
@@ -103,6 +101,5 @@ public class BoardService {
 
         return board.getWriter().equals(login.getId());
     }
-
 
 }
